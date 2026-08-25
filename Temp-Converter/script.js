5e9658c35587afdee8df94ce005058711507f09a
Yashraj Soni <yhrj4874@gmail.com>
2026-08-25 23:33:04 +0530

    Modify error message for unit selection
    Updated message for unselected unit conversion.

--- Temp-Converter/script.js
+++ Temp-Converter/script.js
@@ -14,6 +14,6 @@ function check() {
     temp = temp * 1.8 + 32;
     result.textContent = temp.toFixed(1) + "°C";
   } else {
-    result.textContent = "Select any 1 Unit given above";
+    result.textContent = "Select any 1 Unit to convert";
   }
 }
