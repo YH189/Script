e750ceebd45ff4800a1316db045e15205ca396e4
Yashraj Soni <yhrj4874@gmail.com>
2026-08-25 23:32:18 +0530

    Refactor payment messages for clarity

--- card-payment-form/script.js
+++ card-payment-form/script.js
@@ -9,15 +9,15 @@ submit.onclick = function () {
   if (checkbox.checked) {
     subresult.textContent = `You are subscribed`;
   } else {
-    subresult.textContent = `You need to subscribe first!`;
+    subresult.textContent = `You should subscribe first!`;
   }
   if (visabtn.checked) {
-    submitbtn.textContent = `Payment by Visa`;
+    submitbtn.textContent = `Payment by using Visa`;
   } else if (mastercard.checked) {
-    submitbtn.textContent = `Payment by mastercard`;
+    submitbtn.textContent = `Payment by using mastercard`;
   } else if (Paypl.checked) {
-    submitbtn.textContent = `Payment by Paypal`;
+    submitbtn.textContent = `Payment by using Paypal`;
   } else {
-    submitbtn.textContent = `Choose any of this Card payment!`;
+    submitbtn.textContent = `Choose any of the given Card payment!`;
   }
 };
